f0e51d79d590bc98bf8c6c8e7b14a4763fb1e537
idrisdipo <idriso4@nycstudents.net>
2026-06-08 13:25:58 +0000

    pages

---
+++ project/helper.js
@@ -0,0 +1,14 @@
+function get(id){
+  return document.getElementById(id);
+}
+
+// displatChart( ) accepts the data, an id of the container where to display the chart, and the type of chart to display in the container.
+function displayChart( data, id, type ){
+  let chart = c3.generate({
+    bindto: '#' + id,
+    data: {
+      columns: data,
+      type:type
+    }
+  });
+}
